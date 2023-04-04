3e19c293aa3061291367da5d8843b1266fb9c9b4
Wally Pfingsten <wallypfingsten@gmail.com>
2023-04-04 00:53:54 -0700

    lab complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [number for number in num_list if number % 2 == 0]
+    return even_numbers
 
 def make_exclamation(sentence_list):
-    pass
+    exclaim_list = [exclaim + "!" for exclaim in sentence_list]
+    return exclaim_list
